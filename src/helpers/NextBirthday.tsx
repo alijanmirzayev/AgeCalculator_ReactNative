e8bd6d793ff3de16284f6703ee6d19a52b0e528e
alijanmirzayev <mirzeyevalican1@gmail.com>
2023-07-14 01:36:43 +0400

    updated

--- src/helpers/NextBirthday.tsx
+++ src/helpers/NextBirthday.tsx
@@ -1,21 +1,36 @@
-export const nextBirthday = (dateOfBirth: string) => {
+export const nextBirthday = (dateOfBirthString: string) => {
     // Kullanıcıdan doğum gününü al
-    const userBirthday = new Date(dateOfBirth);
-
-    // Şu anki tarihi al
     const currentDate = new Date();
+    const dateOfBirth = new Date(dateOfBirthString)
 
-    let remainingMonths = userBirthday.getMonth() - currentDate.getMonth();
-    let remainingDays = userBirthday.getDate() - currentDate.getDate();
-
-    if (remainingMonths < 0) {
-        remainingMonths += 12;
+    // Doğum tarihine kalan süreyi hesapla
+    let remainingMonths = currentDate.getMonth() - dateOfBirth.getMonth();
+    let remainingDays = currentDate.getDate() - dateOfBirth.getDate();
+    
+    if (remainingMonths == 0 && remainingDays < 0) {
+        return { remainingMonths, remainingDays: remainingDays * -1 }
     }
 
-    if (remainingDays < 0) {
-        const tempDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0);
-        remainingDays += tempDate.getDate();
-        // remainingMonths--;
+    if (remainingMonths < 0 && remainingDays > 0) {
+        const tempDate = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 0);
+        return { remainingMonths: (remainingMonths + 1) * -1 , remainingDays: tempDate.getDate() - remainingDays }
     }
+
+    if (remainingMonths > 0 && remainingDays < 0) {
+        return { remainingMonths: 12 - remainingMonths, remainingDays: remainingDays * -1 }
+    }
+
+    if (remainingMonths > 0 && remainingDays > 0) {
+        return { remainingMonths: 11 - remainingMonths, remainingDays: 31 - remainingDays }
+    }
+
+    if (remainingMonths > 0 && remainingDays < 0) {
+        return { remainingMonths, remainingDays: remainingDays * -1 }
+    }
+
+    if (remainingMonths < 0 && remainingDays < 0) {
+        return { remainingMonths: remainingMonths * -1, remainingDays: remainingDays * -1 }
+    }
+
     return { remainingMonths, remainingDays }
 }
